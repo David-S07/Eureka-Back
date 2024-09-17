59075203cc085600d2ee64b25242418ff4e7f886
David-S07 <david.tata@hotmail.com>
2024-09-17 19:36:27 -0300

    adicionando campo de cargo

--- login-app-backend-main/src/main/java/com/example/loginauthapi/controllers/AuthController.java
+++ login-app-backend-main/src/main/java/com/example/loginauthapi/controllers/AuthController.java
@@ -45,6 +45,7 @@ public class AuthController {
             newUser.setPassword(passwordEncoder.encode(body.password()));
             newUser.setEmail(body.email());
             newUser.setName(body.name());
+            newUser.setCargo(body.cargo());
             this.repository.save(newUser);
 
             String token = this.tokenService.generateToken(newUser);
