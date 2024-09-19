fbc9a0efe31d5f1127c8dd3bf9d761a206c9d110
David-S07 <david.tata@hotmail.com>
2024-09-19 04:01:28 -0300

    adicionando service, repositori, classe cliente e etc

--- login-app-backend-main/src/main/java/com/example/loginauthapi/domain/user/Cliente.java
+++ login-app-backend-main/src/main/java/com/example/loginauthapi/domain/user/Cliente.java
@@ -2,25 +2,26 @@ package com.example.loginauthapi.domain.user;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Entity
-@Table(name = "users")
+@Table(name = "clientes")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class User {
+public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
     private String email;
-    private String password;
-    private String cargo;
+    private String telefone;
+    private String roteiro;
+    private Date dataCadastro;
     private String status;
 }
